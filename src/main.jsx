bb8edbae7a016bbc09159cdbd74c74372ff7e2a5
Sana-Najjar <najjarsana@hotmail.fr>
2024-06-18 15:32:32 +0200

    add deployement

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import { Provider } from 'react-redux'
-//import {store} from './redux/store'
 import store from './redux/store.js';
 import './index.css'
 import { BrowserRouter } from 'react-router-dom';
